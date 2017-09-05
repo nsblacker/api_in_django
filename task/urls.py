f7c5c5e07130e8e3dbc5882b8a18fb91612414ef
nsblacker <nsblacker@163.com>
2017-09-05 18:39:58 +0800

    django_rest_framework first test
    django api的书写方法

---
+++ task/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from .views import UserViewSet, EntryViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'entries', EntryViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+
+
+
+
